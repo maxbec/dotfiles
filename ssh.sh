fc4c92495990e57f84f4f8b8428444dbfde421ee
Max Beckenbauer <max.bec92@gmail.com>
2023-05-30 22:09:11 +0200

    Changed ssh file

--- ssh.sh
+++ ssh.sh
@@ -10,10 +10,12 @@ ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
 eval "$(ssh-agent -s)"
 
+# If you're using macOS Sierra 10.12.2 or later, you will need to modify your ~/.ssh/config file to automatically load keys into the ssh-agent and store passphrases in your keychain
 touch ~/.ssh/config
 echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
 
-ssh-add -K ~/.ssh/id_ed25519
+# Add your SSH private key to the ssh-agent and store your passphrase in the keychain. If you created your key with a different name, or if you are adding an existing key that has a different name, replace id_ed25519 in the command with the name of your private key file.
+ssh-add --apple-use-keychain ~/.ssh/id_ed25519
 
 # Adding your SSH key to your GitHub account
 # https://docs.github.com/en/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account
