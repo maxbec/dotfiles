8de2b1fef5c76800839195e9031355d94c432470
Max Beckenbauer <max.bec92@gmail.com>
2023-05-31 11:38:21 +0200

    update

--- clone.sh
+++ clone.sh
@@ -2,9 +2,17 @@
 
 echo "Cloning repositories..."
 
+PERSONAL_GITHUB=git@github.com:maxbec
+NAVIGAITE_GITHUB=git@github.com:navigaite
+
 SITES=$HOME/Sites
 LARAVEL=$SITES/Navigaite
 
 # Personal
+git clone $PERSONAL_GITHUB/openHAB-Alignment-Tool.git $SITES/openHAB-Alignment-Tool
+git clone $PERSONAL_GITHUB/AMIGAA-Control-Station.git $SITES/AMIGAA-Control-Station
 
 # Navigaite
+git clone $NAVIGAITE_GITHUB/maimaldrei-mietkatalog.git $SITES/maimaldrei-mietkatalog
+git clone $NAVIGAITE_GITHUB/maimaldrei-importer.git $SITES/maimaldrei-importer
+git clone $NAVIGAITE_GITHUB/navigaite-website.git $SITES/navigaite-website
