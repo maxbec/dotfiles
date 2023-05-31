cbe421a51808eb351a285e22584beedc8fe80b6e
Max Beckenbauer <max.bec92@gmail.com>
2023-05-31 11:55:46 +0200

    update

--- fresh.sh
+++ fresh.sh
@@ -5,8 +5,7 @@ echo "Setting up your Mac..."
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   echo "++++++++++++++++++++++++++++++++++++++"
-  echo "Brew not found. Will be installed now..."
-  echo "++++++++++++++++++++++++++++++++++++++"
+  echo "==> Brew not found. Will be installed now..."
 
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -14,21 +13,18 @@ if test ! $(which brew); then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 else
   echo "++++++++++++++++++++++++++++++++++++++"
-  echo "Brew is already installed!"
-  echo "++++++++++++++++++++++++++++++++++++++"
+  echo "==> Brew is already installed!"
 fi
 
 # Check for Oh My Zsh and install if we don't have it
 if test ! $(which omz); then
   echo "++++++++++++++++++++++++++++++++++++++"
-  echo "OMZ not found. Will be installed now..."
-  echo "++++++++++++++++++++++++++++++++++++++"
+  echo "==> OMZ not found. Will be installed now..."
 
   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 else
   echo "++++++++++++++++++++++++++++++++++++++"
-  echo "OMZ is already installed!"
-  echo "++++++++++++++++++++++++++++++++++++++"
+  echo "==> OMZ is already installed!"
 fi
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
@@ -40,49 +36,37 @@ ln -s .zshrc $HOME/.zshrc
 
 # Update Homebrew recipes
 echo "++++++++++++++++++++++++++++++++++++++"
-echo "Update Homebrew recipes"
-echo "++++++++++++++++++++++++++++++++++++++"
+echo "==> Update Homebrew recipes"
 brew update
 
 # Install all our dependencies with bundle (See Brewfile)
 echo "++++++++++++++++++++++++++++++++++++++"
-echo "Install all our dependencies with bundle (See Brewfile)"
-echo "++++++++++++++++++++++++++++++++++++++"
+echo "==> Install all our dependencies with bundle (See Brewfile)"
 brew tap homebrew/bundle
 brew bundle -v --file ./Brewfile
 
 # Set default MySQL root password and auth type
 echo "++++++++++++++++++++++++++++++++++++++"
-echo "Set default MySQL root password and auth type"
-echo "++++++++++++++++++++++++++++++++++++++"
+echo "==> Set default MySQL root password and auth type"
 mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
 # Create a Sites directory
 echo "++++++++++++++++++++++++++++++++++++++"
-echo "Create a Sites directory"
-echo "++++++++++++++++++++++++++++++++++++++"
+echo "==> Create a Sites directory"
 mkdir $HOME/Sites
-
-# Create sites subdirectories
-echo "++++++++++++++++++++++++++++++++++++++"
-echo "Create sites subdirectories"
-echo "++++++++++++++++++++++++++++++++++++++"
 mkdir $HOME/Sites/Navigaite
 
 # Clone Github repositories
 echo "++++++++++++++++++++++++++++++++++++++"
-echo "Clone Github repositories"
-echo "++++++++++++++++++++++++++++++++++++++"
+echo "==> Clone Github repositories"
 ./clone.sh
 
 # Symlink the Mackup config file to the home directory
 echo "++++++++++++++++++++++++++++++++++++++"
-echo "Symlink the Mackup config file to the home directory"
-echo "++++++++++++++++++++++++++++++++++++++"
+echo "==> Symlink the Mackup config file to the home directory"
 ln -s .mackup.cfg $HOME/.mackup.cfg
 
 # Set macOS preferences - we will run this last because this will reload the shell
 echo "++++++++++++++++++++++++++++++++++++++"
-echo "Set macOS preferences - we will run this last because this will reload the shell"
-echo "++++++++++++++++++++++++++++++++++++++"
+echo "==> Set macOS preferences - we will run this last because this will reload the shell"
 source ./.macos
